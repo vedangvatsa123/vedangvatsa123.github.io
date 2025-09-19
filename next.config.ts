21b2233d4394ae72405cffeecd80f0e4e37e9257
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-09-19 14:32:07 +0000

    make it work at vedangvatsa123.github.io

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/nextn',
-  assetPrefix: '/nextn',
   typescript: {
     ignoreBuildErrors: true,
   },
